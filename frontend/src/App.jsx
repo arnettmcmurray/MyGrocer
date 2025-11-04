09fd9f073792b4c815949745bc813aad1b725810
Arnett McMurray <arnettmcmurray@gmail.com>
2025-11-04 16:37:36 -0600

    feat(frontend): add axios and router, prepare households views

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HouseholdsView from "./views/HouseholdsView";
+import { AuthProvider } from "./context/AuthContext";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/households" element={<HouseholdsView />} />
+          <Route path="*" element={<Navigate to="/households" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
