9778ecc4fc33fbfd05de6f20dd2ad3d70848e98a
Arnett McMurray <arnettmcmurray@gmail.com>
2025-11-11 19:54:37 -0600

    remove unused vite frontend

--- frontend/src/api/households.js
+++
@@ -1,8 +0,0 @@
-import api from "./axios";
-
-export const getHouseholds = () => api.get("/api/v1/households");
-export const getHousehold = (id) => api.get(`/api/v1/households/${id}`);
-export const createHousehold = (data) => api.post("/api/v1/households", data);
-export const updateHousehold = (id, data) =>
-  api.put(`/api/v1/households/${id}`, data);
-export const deleteHousehold = (id) => api.delete(`/api/v1/households/${id}`);
